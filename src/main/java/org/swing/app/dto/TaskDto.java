a1e361af6ba839ca8d8d71f291b0163f3ea4e18c
hoanglong236 <99656288+hoanglong236@users.noreply.github.com>
2023-01-03 07:50:04 +0700

    Add equals method for task dto

--- src/main/java/org/swing/app/dto/TaskDto.java
+++ src/main/java/org/swing/app/dto/TaskDto.java
@@ -1,6 +1,7 @@
 package org.swing.app.dto;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class TaskDto {
 
@@ -101,5 +102,50 @@ public class TaskDto {
         this.note = note;
     }
 
-    // TODO: handle equals method
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof TaskDto) {
+            final TaskDto taskDtoInstance = (TaskDto) obj;
+
+            final boolean idCompare = Objects.equals(taskDtoInstance.id, this.id);
+            if (!idCompare) {
+                return false;
+            }
+            final boolean parentIdCompare = Objects.equals(taskDtoInstance.parentId, this.parentId);
+            if (!parentIdCompare) {
+                return false;
+            }
+            final boolean titleCompare = Objects.equals(taskDtoInstance.title, this.title);
+            if (!titleCompare) {
+                return false;
+            }
+            final boolean importantCompare = taskDtoInstance.important == this.important;
+            if (!importantCompare) {
+                return false;
+            }
+            final boolean startDateTimeCompare = Objects.equals(taskDtoInstance.startDateTime, this.startDateTime);
+            if (!startDateTimeCompare) {
+                return false;
+            }
+            final boolean finishDateTimeCompare = Objects.equals(taskDtoInstance.finishDateTime, this.finishDateTime);
+            if (!finishDateTimeCompare) {
+                return false;
+            }
+            final boolean submitDateTimeCompare = Objects.equals(taskDtoInstance.submitDateTime, this.submitDateTime);
+            if (!submitDateTimeCompare) {
+                return false;
+            }
+            final boolean completedCompare = taskDtoInstance.completed == this.completed;
+            if (!completedCompare) {
+                return false;
+            }
+            final boolean noteCompare = Objects.equals(taskDtoInstance.note, this.note);
+            if (!noteCompare) {
+                return false;
+            }
+
+            return true;
+        }
+        return false;
+    }
 }
